4ea28cdd6a48c5de4ed42c6e46879abe26f7537c
Ryan Hanley <ryanhanley2@gmail.com>
2025-12-07 19:10:00 -0600

    fixing sql

--- last_order_by_customer.sql
+++ last_order_by_customer.sql
@@ -1,16 +1,4 @@
 SELECT
-    customer_id,
-    order_id,
-    order_date,
-    ROW_NUMBER() OVER (
-        PARTITION BY
-            customer_id
-        ORDER BY
-            order_date DESC
-    ) AS rn
-FROM
-    orders
-SELECT
     *
 FROM
     (
